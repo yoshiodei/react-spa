a1537e336bd3bd0be3c2b360ad306c2b7cf0c305
yoshiodei <yoshiodei@yahoo.com>
2021-08-30 23:05:01 +0000

    add About component

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './../css/about.css';
+
+const About = () => {
+    return (
+        <section className="about" id="about">
+            <img className="about_img" />
+            <div className="about_div">
+                <h3>About Us</h3>
+                <p>Amoir hima laamah si madi lodis ini saim moise lameme tou tempora reiciendis nostrum repellendus accusantium eligendi m similique temporibus corporis molestias magni enim. Sint culpa perferendis cumque quasi delectus conse Veniam autem, nulla possimus provident laboriosam tenetur maxime dolorem quaerat odit esse assumenda.</p>
+                <br/>
+                <p>Itaque inventore labore cupiditate enim fugiat odio sunt blanditiis, dolores similique, aliquid quo nemo id sed quisquam? Consequatur veniam quam ipsam vel quo similique tempora nisi, ratione repudiandae quas laudantium.</p>
+                <br/>
+                <p>lodis ini saim moise lameme tou tempora molestias magni enim. Sint culpa perferendis cumque quasi delectus conse Veniam autem, nulla possimus provident laboriosam tenetur maxime</p>
+            </div>
+        </section>
+    );
+}
+
+export default About;
