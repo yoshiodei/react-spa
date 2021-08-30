2c5e708d488efe6d0362d72fe0160f97b5d8aaf5
yoshiodei <yoshiodei@yahoo.com>
2021-08-30 22:08:23 +0000

    add Benefit component

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './../css/benefits.css';
+
+const Benefits = () => {
+    return (
+        <section className="benefits">
+            <div className="benefits_div">
+                <img/>
+                <p>lorem fid iord dior amov raem largh, sahmz hashmaz ni tami sah ma sol maeko lomei nikip mait.</p>
+            </div>
+            <div className="benefits_div">
+                <img/>
+                <p>Mali sorami an aranl yimla samu ov inmus sakla maom tainb sor jim larm ti borgh mahil balur yi.</p>
+            </div>
+            <div className="benefits_div">
+                <img/>
+                <p>largh sahmz hmaz ni lorem fid iord dior amov raem  tami sah ma sol shamsu lomei salamat.</p>
+            </div>
+            <div className="benefits_div">
+                <img/>
+                <p>korim naro bo dior amov raem largh, sahmz hashmaz ni tami sah ma sol koklo rol mait ini.</p>
+            </div>
+        </section>
+    );
+}
+
+export default Benefits;
