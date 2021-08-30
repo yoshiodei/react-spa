b97dd27b448a24f2c98f2a53b3ae4acc5ae083f8
yoshiodei <yoshiodei@yahoo.com>
2021-08-30 20:29:34 +0000

    add Intro.jsx

---
+++ src/components/Intro.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './../css/intro.css';
+
+const Intro = () => {
+    return (
+        <section className="intro">
+            <div className="intro_div">
+                <h2 className="intro_title">Lorem Ipsum Dior</h2>
+                <p className="intro_para">dior ipsum amei larel ukimu samo leid naeh doks bokeh ani.</p>
+                <button className="intro_btn">Read More</button>
+            </div>
+        </section>
+    );
+}
+
+export default Intro;
