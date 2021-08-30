ac1bca367a5b9a72ad4cff65f76c2046a3f19cf0
yoshiodei <yoshiodei@yahoo.com>
2021-08-30 23:32:45 +0000

    add Quote component

---
+++ src/components/Quote.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './../css/quote.css';
+
+const Quote = () => {
+    return (
+        <section className="quote">
+            <div className="quote_div">
+                <h2>Quote</h2>
+                <div className="line"></div>
+                <p>Irum totam unde porro laborum suscipit alias fuga</p>
+                <p>consectetur adipisicing elit. Nostrum totam unde porro laborum suscipit alias fuga aliquid soluta fugit </p>
+                <p>Sint culpa perferendis cumque quasi delectus consectetur totam unde porro labo.</p><br/>
+                <p>- Yoshi Odei</p>
+            </div>
+        </section>
+    );
+}
+
+export default Quote;
