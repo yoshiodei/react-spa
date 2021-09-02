8748a9fab42a40e8c6e5dadd09ea2f87976b454a
yoshiodei <yoshiodei@yahoo.com>
2021-09-02 09:25:24 +0000

    add Contact component

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './../css/contacts.css';
+
+const contacts = () => {
+    return (
+        <section className="contacts">
+            <div className="contacts_div">
+                <div className="contacts_map"></div>
+                <div className="contacts_detail"></div>
+            </div>
+        </section>
+    );
+}
+
+export default contacts;
