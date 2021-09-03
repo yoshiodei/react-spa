27ba599888322bb8747182215041577caeb832ad
yoshiodei <yoshiodei@yahoo.com>
2021-09-03 12:35:33 +0000

    add Nav Component

--- src/components/NavAlt.jsx
+++ src/components/NavAlt.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import './../css/nav.css';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 
-const Nav = ({ items }) => {
+const NavAlt = ({ items }) => {
     return (
         <nav className="nav">
             <h2 className="nav_brand">Yoshi's Spa &amp; Wellness Centre</h2>
             <ul className="nav_menu-list">
-                <li><a href="">About</a></li>
-                <li><a href="">Services</a></li>
-                <li><a href="">Products</a></li>
-                <li><a href="">Contact</a></li>
+                <li className="cart_link" ><Link to="/">Home</Link></li>
                 <li className="cart_link" ><span>{items.length}</span><Link to="/cart">Cart</Link></li>
             </ul>
             <div className="nav_hamburger">
@@ -28,4 +24,4 @@ const mapStateToProps = (state) => {
     return { items:state.cart_items }
 }
 
-export default connect(mapStateToProps)(Nav);
+export default connect(mapStateToProps)(NavAlt);
