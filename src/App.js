b3ee53214224755b0b7dff8bdaf1ab03d9a37582
yoshiodei <yoshiodei@yahoo.com>
2021-09-03 12:39:18 +0000

    modify App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from "./Pages/Home";
+import Footer from "./components/Footer";
+import ProductPageCreams from "./Pages/ProductPageCreams";
+import CartPage from "./Pages/CartPage";
+import ProductPageSoaps from "./Pages/ProductPageSoaps";
+import ProductPageOils from "./Pages/ProductPageOils";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          <h2 className="message">Page Is Available For Large Screens Only</h2>
+          
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/product/creams/:id" exact>
+              <ProductPageCreams />
+            </Route>
+            <Route path="/product/oils/:id" exact>
+              <ProductPageOils />
+            </Route>
+            <Route path="/product/soaps/:id" exact>
+              <ProductPageSoaps />
+            </Route>
+            <Route path="/cart" exact>
+              <CartPage />
+            </Route>
+          </Switch>
+          <Footer />
+        </Router>
     </div>
   );
 }
